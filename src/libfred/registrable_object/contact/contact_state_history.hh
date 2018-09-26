7ff40002b24975a660cc0c9f9c0a0967ef3d0a0b
Michal Strnad <michal.strnad@nic.cz>
2018-09-26 15:25:51 +0200

    Ticket #22744 - Add ContactStateHistory type

--- src/libfred/registrable_object/contact/contact_state_history.hh
+++ src/libfred/registrable_object/contact/contact_state_history.hh
@@ -16,26 +16,20 @@
  * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef GET_CONTACT_STATE_HISTORY_HH_7827DF8F59926B83B7C006F9DD976553//date "+%s.%N"|md5sum|tr "[a-f]" "[A-F]"
-#define GET_CONTACT_STATE_HISTORY_HH_7827DF8F59926B83B7C006F9DD976553
+#ifndef CONTACT_STATE_HISTORY_HH_4A3B56DF43A8017423C215283116A15C//date "+%s.%N"|md5sum|tr "[a-f]" "[A-F]"
+#define CONTACT_STATE_HISTORY_HH_4A3B56DF43A8017423C215283116A15C
 
 #include "libfred/registrable_object/get_state_history.hh"
-#include "libfred/registrable_object/operation_by_handle.hh"
-#include "libfred/registrable_object/operation_by_id.hh"
-#include "libfred/registrable_object/operation_by_uuid.hh"
 #include "libfred/registrable_object/contact/contact_state.hh"
-#include "libfred/registrable_object/contact/contact_state_history.hh"
 
 namespace LibFred {
 namespace RegistrableObject {
 namespace Contact {
 
-using GetContactStateHistoryById = OperationById<GetStateHistory, ContactState>;
-using GetContactStateHistoryByHandle = OperationByHandle<GetStateHistory, ContactState>;
-using GetContactStateHistoryByUUID = OperationByUUID<GetStateHistory, ContactState>;
+using ContactStateHistory = StateHistory<ContactState>;
 
 }//namespace LibFred::RegistrableObject::Contact
 }//namespace LibFred::RegistrableObject
 }//namespace LibFred
 
-#endif//GET_CONTACT_STATE_HISTORY_HH_7827DF8F59926B83B7C006F9DD976553
+#endif//CONTACT_STATE_HISTORY_HH_4A3B56DF43A8017423C215283116A15C
