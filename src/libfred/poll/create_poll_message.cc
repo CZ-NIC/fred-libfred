a5a29c3540f14ed7d5b5c511b46462da1659b491
Jiri Sadek <jiri.sadek@nic.cz>
2019-07-17 15:07:33 +0200

    Ticket #25396 - Primary poll message recipient in separate function

--- src/libfred/poll/create_poll_message.cc
+++ src/libfred/poll/create_poll_message.cc
@@ -193,85 +193,95 @@ const CreatePollMessageFunction MessageTypeTraits<MessageType::delete_domain>::c
         create_poll_eppaction_message<message_type>;
 
 template <SponsoringRegistrar::Enum recipient>
-struct GetRecipient { };
+struct GetPrimaryRecipientImpl { };
 
 template <>
-struct GetRecipient<SponsoringRegistrar::who_did_the_action>
+struct GetPrimaryRecipientImpl<SponsoringRegistrar::who_did_the_action>
 {
-    static const char sql[];
-};
-
-template <>
-struct GetRecipient<SponsoringRegistrar::at_the_transfer_start>
-{
-    static const char sql[];
-};
-
-const char GetRecipient<SponsoringRegistrar::who_did_the_action>::sql[] =
+    static constexpr char sql[] =
         "SELECT eot.id IS NOT NULL,oh.clid "
         "FROM object_history oh "
         "JOIN object_registry obr ON obr.id=oh.id "
         "LEFT JOIN enum_object_type eot ON eot.id=obr.type AND eot.name=$2::TEXT "
         "WHERE oh.historyid=$1::BIGINT";
 
-const char GetRecipient<SponsoringRegistrar::at_the_transfer_start>::sql[] =
+};
+
+constexpr char GetPrimaryRecipientImpl<SponsoringRegistrar::who_did_the_action>::sql[];
+
+template <>
+struct GetPrimaryRecipientImpl<SponsoringRegistrar::at_the_transfer_start>
+{
+    static constexpr char sql[] =
         "SELECT eot.id IS NOT NULL,oh.clid "
         "FROM object_history oh "
         "JOIN object_registry obr ON obr.id=oh.id "
         "LEFT JOIN enum_object_type eot ON eot.id=obr.type AND eot.name=$2::TEXT "
         "WHERE oh.historyid=(SELECT id FROM history WHERE next=$1::BIGINT)";
+};
+
+constexpr char GetPrimaryRecipientImpl<SponsoringRegistrar::at_the_transfer_start>::sql[];
+
+template<SponsoringRegistrar::Enum recipient, Object_Type::Enum object_type>
+struct GetPrimaryRecipient
+{
+    unsigned long long exec(LibFred::OperationContext& _ctx, unsigned long long _history_id) const
+    {
+        const std::string requested_object_type_handle = Conversion::Enums::to_db_handle(object_type);
+        const Database::Result db_res =
+                _ctx.get_conn().exec_params(
+                        GetPrimaryRecipientImpl<recipient>::sql,
+                        Database::query_param_list(_history_id)(requested_object_type_handle));
+
+        switch (db_res.size())
+        {
+            case 0:
+            {
+                struct NotFound : OperationException
+                {
+                    const char* what() const noexcept { return "object history not found"; }
+                };
+                throw NotFound();
+            }
+            case 1:
+                break;
+            default:
+            {
+                struct TooManyRows : InternalError
+                {
+                    TooManyRows() : InternalError("too many rows") { }
+                };
+                throw TooManyRows();
+            }
+        }
+
+        const bool object_type_corresponds_to_message_type = static_cast<bool>(db_res[0][0]);
+        if (!object_type_corresponds_to_message_type)
+        {
+            struct NotCorrespondingObjectType : OperationException
+            {
+                const char* what() const noexcept
+                {
+                    return "associated object is not of the type corresponding to the given message type";
+                }
+            };
+            throw NotCorrespondingObjectType();
+        }
+
+        return static_cast<unsigned long long>(db_res[0][1]);
+    }
+};
+
 
 } // namespace LibFred::Poll::{anonymous}
 
 template <MessageType::Enum message_type>
 unsigned long long CreatePollMessage<message_type>::exec(
         LibFred::OperationContext& _ctx,
-        unsigned long long _history_id)const
+        unsigned long long _history_id) const
 {
     typedef MessageTypeTraits<message_type> MessageTraits;
-    const std::string requested_object_type_handle =
-            Conversion::Enums::to_db_handle(MessageTraits::object_type);
-    const Database::Result db_res =
-            _ctx.get_conn().exec_params(
-                    GetRecipient<MessageTraits::recipient>::sql,
-                    Database::query_param_list(_history_id)(requested_object_type_handle));
-
-    switch (db_res.size())
-    {
-        case 0:
-        {
-            struct NotFound:OperationException
-            {
-                const char* what()const noexcept { return "object history not found"; }
-            };
-            throw NotFound();
-        }
-        case 1:
-            break;
-        default:
-        {
-            struct TooManyRows:InternalError
-            {
-                TooManyRows():InternalError("too many rows") { }
-            };
-            throw TooManyRows();
-        }
-    }
-
-    const bool object_type_corresponds_to_message_type = static_cast<bool>(db_res[0][0]);
-    if (!object_type_corresponds_to_message_type)
-    {
-        struct NotCorrespondingObjectType:OperationException
-        {
-            const char* what()const noexcept
-            {
-                return "associated object is not of the type corresponding to the given message type";
-            }
-        };
-        throw NotCorrespondingObjectType();
-    }
-
-    const unsigned long long recipient_registrar_id = static_cast<unsigned long long>(db_res[0][1]);
+    const auto recipient_registrar_id = GetPrimaryRecipient<MessageTraits::recipient, MessageTraits::object_type>().exec(_ctx, _history_id);
     return MessageTraits::create_poll_message(_ctx, _history_id, recipient_registrar_id);
 }
 
